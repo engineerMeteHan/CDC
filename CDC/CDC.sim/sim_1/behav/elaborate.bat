87855f11b0478966bb7b1f5dbd0926e062598205
engineerMeteHan <metehangencereem@example.com>
2025-11-01 23:59:33 +0300

    First Commit. Upload GitHub

---
+++ CDC/CDC.sim/sim_1/behav/elaborate.bat
@@ -0,0 +1,9 @@
+@echo off
+set xv_path=D:\\Xilinx_Vivado\\Vivado\\2016.1\\bin
+call %xv_path%/xelab  -wto 7ed3e1c6053349a3a8715dbbc5f98464 -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L secureip --snapshot tb_clks_behav xil_defaultlib.tb_clks -log elaborate.log
+if "%errorlevel%"=="0" goto SUCCESS
+if "%errorlevel%"=="1" goto END
+:END
+exit 1
+:SUCCESS
+exit 0
